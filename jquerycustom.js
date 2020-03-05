e95b4b5dfad34c020a0cb05913108aaae04863b8
gear5km <https://github.com/gear5km/Project-Reality-Arcade/tree/v0.1>
2020-03-05 15:49:45 +0300

    0.7

--- jquerycustom.js
+++ jquerycustom.js
@@ -1,9 +1,49 @@
 jQuery(document).ready(function(){
 
-    $('.button1').click(function showFace(){
-        $('#ProductBox1').show();
+    $('#faceButton').click(function(){
+
+        $('.Products').hide();
+        $('#eyes1').hide();
+        $('#lips1').hide();
+        $('#skin1').hide();
+
+        $('#face1').show();
+    })
+
+    $('#eyesButton').click(function showFace(){
+        
+        $('.Products').hide();
+        $('#face1').hide();
+        $('#lips1').hide();
+        $('#skin1').hide();
+
+        $('#eyes1').show();
 
     })
+
+    $('#lipsButton').click(function showFace(){
+        
+        $('.Products').hide();
+        $('#face1').hide();
+        $('#eyes1').hide();
+        $('#skin1').hide();
+
+        $('#lips1').show();
+
+    })
+
+    $('#skinButton').click(function showFace(){
+        
+        $('.Products').hide();
+        $('#face1').hide();
+        $('#eyes1').hide();
+        $('#lips1').hide();
+
+        $('#skin1').show();
+
+    })
+
+    
 
 
 
