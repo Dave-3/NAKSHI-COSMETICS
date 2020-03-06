bdefdcea3a467d7033fbe82e9ebd2f69b058bb83
gear5km <60645081+gear5km@users.noreply.github.com>
2020-03-06 11:20:12 +0300

    Update javascriptcustom.js

--- javascriptcustom.js
+++ javascriptcustom.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 //function showFace(){
 //    alert ("function showFace is running");
 
-=======
+
 //function showFace(){
 //    alert ("function showFace is running");
 
->>>>>>> 36661e4a34854459d10df5709eed3b44f6a82b95
+
 //}
